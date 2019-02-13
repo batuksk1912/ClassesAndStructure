7a5a37af1220d35192c15b8e83d59970d61f03cf
Baturay Kayaturk <baturaykayaturk@hotmail.com>
2019-02-13 18:30:26 -0500

    Initial Commit

---
+++ ClassesAndStructure/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ClassesAndStructure
+//
+//  Created by Baturay Kayatürk on 13.02.2019.
+//  Copyright © 2019 LambtonCollege. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
